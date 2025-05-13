1e6a39a34bca083fa769c0e472b21fce7b4a9c53
ChilliPhantom <129586206+ChilliPhantom@users.noreply.github.com>
2025-05-13 18:22:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  // other valid options
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
