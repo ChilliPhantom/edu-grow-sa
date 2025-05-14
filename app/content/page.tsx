2d32006089880008c824f48cb5c2e7186cad2c3a
ChilliPhantom <129586206+ChilliPhantom@users.noreply.github.com>
2025-05-14 21:53:52 +0200

    Update page.tsx

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,12 +1,3 @@
-//'use client';
-
-import React from 'react';
-
 export default function ContentPage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-3xl font-bold">Educational Content</h1>
-      <p className="mt-2">This is the content page.</p>
-    </main>
-  );
+  return <div>Hello</div>;
 }
