f09bba29b69d618af83879426cadee4fdd4bc4aa
ChilliPhantom <129586206+ChilliPhantom@users.noreply.github.com>
2025-05-16 13:34:47 +0200

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
+'use client';
+
+import * as React from 'react';
 
 export const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ children, ...props }, ref) => {
-  return (
-    <div ref={ref} {...props} className="p-4 border rounded shadow bg-white">
-      {children}
-    </div>
-  );
-});
+>(({ children, ...props }, ref) => (
+  <div ref={ref} {...props} className="p-4 border rounded shadow bg-white">
+    {children}
+  </div>
+));
 
-Card.displayName = "Card";
+Card.displayName = 'Card';
